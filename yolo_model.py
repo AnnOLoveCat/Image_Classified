a49464b5eebc379fb91444806ea4e699ff5b6b81
AnnU <43593864+AnnOLoveCat@users.noreply.github.com>
2026-02-18 17:37:22 +0800

    update: remove pyproject.toml and uv.lock work, use yolov8s prevent server crashes

--- yolo_model.py
+++ yolo_model.py
@@ -10,7 +10,7 @@ import os
 @st.cache_resource
 def load_model():
     # 使用 YOLO-World 模型
-    model_world = YOLOWorld("yolov8x-worldv2.pt")
+    model_world = YOLOWorld("yolov8s-worldv2.pt")
     # 預設標籤，避免初始化報錯
     model_world.set_classes(["object", "item", "person", "tool", "equipment", "facility", "structure"])
     return model_world
@@ -77,7 +77,7 @@ def main():
                     fmt = "onnx" if "ONNX" in export_format else "engine"
                     
                     # 執行匯出
-                    path = model.export(format=fmt) 
+                    path = model.export(format=fmt, simplify=False) 
                     
                     # 成功後，將路徑存入 session_state，這樣重整後下載按鈕才不會消失
                     st.session_state['export_file'] = path
